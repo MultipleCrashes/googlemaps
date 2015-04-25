922436f04c949437a1fbb4ba337878dc2c5622d7
Harish Kumar Upadhyay <inimitableharish@gmail.com>
2015-04-25 20:16:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,14 @@ def index():
 	obj=GoogleMapRelated.GoogleMapsClass()
         lat_long=obj.getLocation(address="BTM Stage 2 Bangalore karnataka") 
         	
-	obj.getNearbyPlaces(lat=lat_long["lat"],long=lat_long["lng"],type='school',name='school',key='AIzaSyCxuL9ReVu19BS2ZaFuMxB-3E2uPVo2ZHM')
-	return "Latitued and Longitude of the place is "+str(lat_long)
+	nearByPlaces=obj.getNearbyPlaces(lat=lat_long["lat"],long=lat_long["lng"],type='school',name='school',key='AIzaSyCxuL9ReVu19BS2ZaFuMxB-3E2uPVo2ZHM')
+	return "Latitued and Longitude of the location  "+str(lat_long)+ str(nearByPlaces)
+	
+
 
 if __name__=='__main__':
 	app.run(debug=True)
+
 
 
 
