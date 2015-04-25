ddaf43225625e7852b35d962c3b454fc871a51a1
Harish Kumar Upadhyay <inimitableharish@gmail.com>
2015-04-25 19:45:25 +0530

    Added a function to find and locate nearby places.

--- GoogleMapRelated.py
+++ GoogleMapRelated.py
@@ -1,4 +1,3 @@
-
 ''' @author : Harish '''
 
 import os 
@@ -8,25 +7,46 @@ import ast
 import time 
 
 
-class GoogleMapsRelated:
+class GoogleMapsClass:
 		def __init__(self,clientKey="AIzaSyCxuL9ReVu19BS2 \
                                             ZaFuMxB-3E2uPVo2ZHM"):
 			self.clientKey=clientKey
 		        	
 		def getLocation(self,address=None):
+			'''Function to get latitue and longitude of an address'''
+
 			r=requests.get('https://maps.googleapis.com/maps/api/geocode/json?address='   \
 					+address+'&key=AIzaSyCxuL9ReVu19BS2ZaFuMxB-3E2uPVo2ZHM')
 			responseString=json.dumps(json.loads(r.text))
 			time.sleep(2)
 			resp=ast.literal_eval(responseString)		
 			
-			print "The Latitude and Longitude for the given address is "+str(resp['results'][0]['geometry']['location'])
+			#print "The Latitude and Longitude for the given address is "+str(resp['results'][0]['geometry']['location'])
 			return resp['results'][0]['geometry']['location']
 
 
 
+		def getNearbyPlaces(self,lat=12.9,long=77.6,type="restaurant",name="None",radius=500,key='AIzaSyCxuL9ReVu19BS2ZaFuMxB-3E2uPVo2ZHM'):
+	
+			'''Function to get different types of things in certain areas'''
+			print "The place name is-> ",name
+			placesQuery="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+   \
+				     str(lat)+","+str(long)+"&radius="+str(radius)+"&types="+str(type)+"&name="+  \
+                                     str(name)+"&key="+str(key)
+			#placesQuery="https://maps.googleapis.com/maps/api/place/nearbysearch/json?"+str(lat)+","+str(long)
+			print "---------->",placesQuery
+			#placesQuery="https://www.google.co.in/maps/search/json/"+placeName+"/@"+str(lat)+","+str(long)+",1z"	
+			resp=requests.get(placesQuery)
+			print "The places near the current place is --->", str(json.dumps(json.loads(resp.text)))
+			
+			
+			
 
+'''
 
 if __name__=='__main__':
-	obj=GoogleMapsRelated()
-	obj.getLocation(address="1600+Amphitheatre+Parkway,+Mountain+View,+CA+")
+	obj=GoogleMapsClass()
+	obj.getNearbyPlaces(lat=12.9,long=77.6,placeName='school')
+	#obj.getLocation(address="Domluru bangalore karnataka")
+
+'''
