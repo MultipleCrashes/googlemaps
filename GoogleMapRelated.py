7e629ea54c89fb9d51c6a419ef9bd76ed3e4d5fd
Harish Kumar Upadhyay <inimitableharish@gmail.com>
2015-04-25 16:16:34 +0530

    Create GoogleMapRelated.py

---
+++ GoogleMapRelated.py
@@ -0,0 +1,23 @@
+
+
+
+import os 
+import requests
+
+
+
+class GoogleMapsRelated:
+		def __init__(self,clientKey="AIzaSyCxuL9ReVu19BS2 \
+                                            ZaFuMxB-3E2uPVo2ZHM"):
+			self.clientKey=clientKey
+		        	
+		def getLocation(self,address=None):
+			print "Harish"
+
+
+
+
+
+if __name__=='__main__':
+	obj=GoogleMapsRelated()
+	obj.getLocation()
