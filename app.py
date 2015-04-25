baecee17f8bb1f35138760edc43ca58272653025
Harish Kumar Upadhyay <inimitableharish@gmail.com>
2015-04-25 19:44:15 +0530

    Corrected a variable name

--- app.py
+++ app.py
@@ -10,10 +10,13 @@ def index():
 	obj=GoogleMapRelated.GoogleMapsClass()
         lat_long=obj.getLocation(address="BTM Stage 2 Bangalore karnataka") 
         	
-	obj.getNearbyPlaces(lat=lat_long["lat"],long=77.7,type='school',name='school',key='AIzaSyCxuL9ReVu19BS2ZaFuMxB-3E2uPVo2ZHM')
+	obj.getNearbyPlaces(lat=lat_long["lat"],long=lat_long["lng"],type='school',name='school',key='AIzaSyCxuL9ReVu19BS2ZaFuMxB-3E2uPVo2ZHM')
 	return "Latitued and Longitude of the place is "+str(lat_long)
 
 if __name__=='__main__':
 	app.run(debug=True)
+
+
+
 
 
